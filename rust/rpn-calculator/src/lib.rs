b5502866ff960670b5d4e986cf7a7fda794b5ad9
Bob Allen <bogghead@gmail.com>
2022-08-06 09:32:28 -0400

    Reverse string done in 1-line method

---
+++ rust/rpn-calculator/src/lib.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+pub enum CalculatorInput {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Value(i32),
+}
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    inputs
+        .iter()
+        .try_fold::<_, _, Option<_>>(Vec::<i32>::new(), |mut stack, item| {
+            let result: i32 = if let CalculatorInput::Value(v) = item {
+                *v
+            } else {
+                let b = stack.pop()?;
+                let a = match stack.pop() {
+                    Some(it) => it,
+                    None => return None,
+                };
+                let op = match item {
+                    CalculatorInput::Add => std::ops::Add::add,
+                    CalculatorInput::Subtract => std::ops::Sub::sub,
+                    CalculatorInput::Multiply => std::ops::Mul::mul,
+                    CalculatorInput::Divide => std::ops::Div::div,
+                    CalculatorInput::Value(_) => unreachable!(),
+                };
+                op(a, b)
+            };
+            stack.push(result);
+            Some(stack)
+        })
+        .and_then(|stack| {
+            if stack.len() == 1 {
+                Some(stack[0])
+            } else {
+                None
+            }
+        })
+}
